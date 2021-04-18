64cd78315732d11b68ab46c81a159d36992462f2
Lucas Winther <lucasw89@live.dk>
2021-04-18 17:52:02 +0200

    Now check for games of same title yay.

--- PowerShellScript.ps1
+++ PowerShellScript.ps1
@@ -8,36 +8,32 @@ function global:OnApplicationStopped()
 
 function global:ImportCategories()
 {
-    $games = $PlayniteApi.Database.Games
+    # Import TAB delimited file
+    $csvData = Import-Csv -Delimiter "`t" -Path G:\Projects\github-repos\playnite-galaxy2-categories-importer\gameDB.csv
 
+    $PlayniteApi.Dialogs.ShowMessage("Loaded csv")
+    
     $testcat = New-Object Playnite.SDK.Models.Category('test')
     $PlayniteApi.Database.Categories.Add($testcat)
     $PlayniteApi.Dialogs.ShowMessage("Created new category")
 
+    $games = $PlayniteApi.Database.Games
     foreach ($game in $games) {
-        if ($game.Name -eq "20XX") {
-            $PlayniteApi.Dialogs.ShowMessage($game.Name)
-
-            if ($game.CategoryIds -eq $null){
-                $game.CategoryIds = New-Object System.Collections.Generic.List[System.Guid]
+        $searchTerm = $game.Name
+        $temp = $csvData | Where-Object {$_.title -match $searchTerm}
+        if ($temp -ne $null){
+            # Found something good :)
+            if ($temp.tags -ne ""){
+                $PlayniteApi.Dialogs.ShowMessage($temp.title)
+                $PlayniteApi.Dialogs.ShowMessage($temp.tags)
             }
-            $game.CategoryIds.Add($testcat.Id)
-            
         }
     }
+    $PlayniteApi.Dialogs.ShowMessage($noOfEqualities)
+
 
     $PlayniteApi.Dialogs.ShowMessage("Search is done")
 }
-
-function global:GetCsv()
-{
-    # Import TAB delimited file
-    $data = Import-Csv -Delimiter "`t" -Path G:\Projects\github-repos\playnite-galaxy2-categories-importer\gameDB.csv
-    $searchTerm = "20XX"
-    $test = $data | Where-Object {$_.title -match $searchTerm}
-    $PlayniteApi.Dialogs.ShowMessage($test.title)
-}
-
 
 function global:GetMainMenuItems()
 {
@@ -45,8 +41,7 @@ function global:GetMainMenuItems()
 
     $menuItem = New-Object Playnite.SDK.Plugins.ScriptMainMenuItem
     $menuItem.Description = "Import GOG 2.0 Categories..."
-    #$menuItem.FunctionName = "ImprotCategories"
-    $menuItem.FunctionName = "GetCsv"
+    $menuItem.FunctionName = "ImportCategories"
     $menuItem.MenuSection = "@"
     return $menuItem
 }
